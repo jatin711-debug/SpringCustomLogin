aa727ed02886b8e49657dc359be7cdf9a4e5c168
jatin711-debug <69040077+jatin711-debug@users.noreply.github.com>
2021-07-25 18:40:23 -0400

    addin navigation,securityconfig,login and others.

--- src/main/java/com/sheridan/security/SecurityConfig.java
+++ src/main/java/com/sheridan/security/SecurityConfig.java
@@ -1,17 +1,36 @@
 package com.sheridan.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
-public class SecurityConfig {
-    @SuppressWarnings("deprecated")
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder authentication) throws Exception {
-        authentication.
-                        inMemoryAuthentication()
-                            .passwordEncoder(NoOpPasswordEncoder.getInstance())
-                                .withUser("jatin").password("jatin711").roles("user");
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+        .antMatchers("/secure/**").hasRole("USER")
+            .antMatchers("/", "/js/**", "/css/**", "/images/**", "/permission-denied").permitAll()
+                .antMatchers("/**").denyAll()
+                    .anyRequest().authenticated()
+                    .and().formLogin()
+                        .loginPage("/login").permitAll()
+                            .and().logout()
+                                .invalidateHttpSession(true)
+                                    .clearAuthentication(true)
+                                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                                            .logoutSuccessUrl("/login?logout").permitAll();
+
     }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication()
+        .passwordEncoder(NoOpPasswordEncoder.getInstance())
+            .withUser("frank@frank.com").password("1234").roles("USER")
+                .and().withUser("guest@guest.com").password("password").roles("GUEST");
+}
 }
